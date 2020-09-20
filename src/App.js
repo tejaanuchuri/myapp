28a0a3969bab042e0f38b41a9389ab7d922ed8a4
tejaanuchuri <tejaanuchuri0009@gmail.com>
2020-09-20 21:19:52 +0530

    "using the useState in hook for state maniplation"

--- src/App.js
+++ src/App.js
@@ -1,39 +1,41 @@
-import React, { Component} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person'
 
-class App extends Component {
-  state = {
-    person:[
+const App = props => {
+  const [personState, setPersonState] =  useState({
+    person: [
       { name : 'teja' },
       { name : 'suresh' },
       { name : 'jnani' }
     ]
-  }
-  switchNameHandler = () => {
-    //  console.log('was clicked...!');
-    // don't do thid=>  this.state.person[0].name = 'jnani';
+  });  
 
-    this.setState({
-      person:[
-         { name : 'jnani' },
-         { name : 'teja' },
-         { name : 'suresh' }
-      ]
-    })
-  } 
-  render() {
-    return(
+console.log(personState);
+const switchNameHandler = () => {
+  //  console.log('was clicked...!');
+  // don't do thid=>  this.state.person[0].name = 'jnani';
+
+ setPersonState({
+    person:[
+       { name : 'jnani' },
+       { name : 'teja' },
+       { name : 'suresh' }
+    ]
+  });
+}; 
+  return(
       <div className="App">
-      <h1> Hi, I'm React App </h1>
-      <p> This is Really Cool..! </p>
-      <button onClick={this.switchNameHandler}>Switch Name</button>
-      <Person name={this.state.person[0].name}> I'm computer science graduated...!</Person>
-      <Person name ={this.state.person[1].name}/>
-      <Person name ={this.state.person[2].name}/>    
+        <h1> Hi, I'm React App </h1>
+        <p> This is Really Cool..! </p>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person name={personState.person[0].name}> I'm computer science graduated...!</Person>
+      <Person name ={personState.person[1].name}/>
+      <Person name ={personState.person[2].name}/>    
       </div>
-    ); 
-  }
+    );
 }
 
 export default App;
+
+
