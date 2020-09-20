85ee2125fff8d39aa034de38e729cec500af30b2
tejaanuchuri <tejaanuchuri0009@gmail.com>
2020-09-20 19:46:48 +0530

    "understanding using state"

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    person:[
+      { name : 'teja' },
+      { name : 'suresh' },
+      { name : 'jnani' }
+    ]
+  } 
   render() {
     return(
       <div className="App">
       <h1> Hi, I'm React App </h1>
       <p> This is Really Cool..! </p>
-      <Person name="Teja"> I'm computer science graduated...!</Person>
-      <Person name ="suresh"/>
-      <Person name ="shiva" />    
+      <button>Switch Name</button>
+      <Person name={this.state.person[0].name}> I'm computer science graduated...!</Person>
+      <Person name ={this.state.person[1].name}/>
+      <Person name ={this.state.person[2].name}/>    
       </div>
     ); 
   }
