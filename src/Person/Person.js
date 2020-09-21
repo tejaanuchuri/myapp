5fd08ac8a9cd7a30d27c505837d0cbc58fa8b72f
tejaanuchuri <tejaanuchuri0009@gmail.com>
2020-09-21 11:58:57 +0530

    "adding styling to our componets"

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Person.css';
 const person  = (props) => {
     return(
-        <div>
+        <div className="Person">
             <p onClick={props.click}> I'm a {props.name}..! and I am {Math.floor(Math.random()*30)} years old.  </p>
             <p>{props.children}</p>
-            <input type="text" onChange={props.changed} value = {props.name}/>
+            <input type="text" onChange={props.changed} />
         </div>
     )
 }
