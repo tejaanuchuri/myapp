ca6554e772437a5fee689d7484d4f6ed17cd0a8c
tejaanuchuri <tejaanuchuri0009@gmail.com>
2020-09-21 11:23:40 +0530

    "using bind method to pass props"

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component{
     ]
   });  
 
-switchNameHandler = () => {
+switchNameHandler = (newName) => {
   //  console.log('was clicked...!');
   // don't do thid=>  this.state.person[0].name = 'jnani';
 
@@ -19,7 +19,7 @@ switchNameHandler = () => {
     person: [
        { name : 'jnani' },
        { name : 'teja' },
-       { name : 'suresh' }
+       { name : newName }
     ]
   })
 }
@@ -28,8 +28,8 @@ switchNameHandler = () => {
       <div className="App">
         <h1> Hi, I'm React App </h1>
         <p> This is Really Cool..! </p>
-      <button onClick={this.switchNameHandler}>Switch Name</button>
-      <Person name={this.state.person[0].name} click = {this.switchNameHandler}> I'm computer science graduated...!</Person>
+      <button onClick={this.switchNameHandler.bind(this,'kiduo')}>Switch Name</button>
+      <Person name={this.state.person[0].name} click = {this.switchNameHandler.bind(this,'gojenjiii')}> I'm computer science graduated...!</Person>
       <Person name ={this.state.person[1].name}/>
       <Person name ={this.state.person[2].name}/>    
       </div>
